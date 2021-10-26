1c80f703b436f4e552e020402d9bfd1bff0ce477
geroblanco <geroblanco13@gmail.com>
2021-10-25 21:04:29 -0300

    Cambios sobre bandas terminado

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
 
-
-
-
 // Path Publico 
 const publicPath = path.resolve(__dirname, 'public');
 
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,20 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen 2'));
+bands.addBand(new Band('Bad buuny perro'));
+bands.addBand(new Band('BB king jajaja'));
+bands.addBand(new Band('parcels capos'));
+
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -11,5 +23,30 @@ io.on('connection', client => {
     client.on('mensaje', (payload) => {
         console.log('mensajee!!!!', payload);
         io.emit('mensaje', {admin: 'Nuevo mensaje'});
-    })
+    });
+
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    // Escuchar add-band
+
+    client.on('add-band', (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    // Borrar delete-band
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    // client.on('emitir-mensaje', (payload) =>{
+    //     // console.log(payload);
+    //     client.broadcast.emit('nuevo-mensaje', payload);
+    // })
 });
